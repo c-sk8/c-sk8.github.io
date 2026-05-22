957c18cff92d4d11dfc407b20d82ce5d2890a70f
c-sk8 <37511949+c-sk8@users.noreply.github.com>
2026-05-22 12:08:53 +0100

    Copied files from projects repository

---
+++ marching-cubes-demo/hud.js
@@ -0,0 +1,31 @@
+// =====================================================================================
+// Copyright (C) 2026 Christopher Kent http://c-sk8.github.io
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, version 3.
+// =====================================================================================
+
+import {	fieldIndex } from './event-process.js';
+import {	getColourModeName,  getColourModeIndexString} from './colour-modes.js';
+import {	getFieldIndexString, getFieldName, getVariantIndexString, getColourMode }
+		 from './field-functions-manager.js';
+
+export function updateHUD() {
+    document.getElementById("surfaceIndex").textContent = getFieldIndexString(fieldIndex);
+    document.getElementById("surfaceName").textContent = getFieldName(fieldIndex);
+    document.getElementById("variantIndex").textContent = getVariantIndexString(fieldIndex);
+    document.getElementById("colourModeIndex").textContent = getColourModeIndexString(getColourMode(fieldIndex));
+    document.getElementById("colourMode").textContent = getColourModeName(getColourMode(fieldIndex));
+}
+
+export function updateVertexCount(vertexCount) {
+	document.getElementById("vertexCount").textContent = vertexCount;
+}
+
+export function updateSurfaceGenerationTime(duration) {
+	document.getElementById("surfGenTime").textContent = `${duration.toFixed(0)} ms`;
+}
+
+export function clearSurfaceGenerationTime() {
+	document.getElementById("surfGenTime").textContent = "";
+}
